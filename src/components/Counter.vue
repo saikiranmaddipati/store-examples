6dce47634399a3dfdc78fb85525dbf0f999c99bd
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-09-13 12:34:58 +0530

    vuex examples

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <p>{{ storeCount}}</p>
+  <p>
+    <button @click="increment">+</button>
+    <button @click="decrement" class="button">-</button>
+  </p>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'CounterPage',
+  computed: {
+    storeCount () {
+      return this.$store.getters.storeCount
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+    .button{
+        margin-left: 10px;
+    }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 22
   },
   getters: {
+    storeCount: state => state.count
   },
   mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
   },
   actions: {
   },
